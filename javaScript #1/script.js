c705c10c1297d4b9d9b8fb5a0c3f642e18dd4374
NikaBeglarashvili <129869889+NikaBeglarashvili@users.noreply.github.com>
2023-05-15 18:32:56 +0400

    Add files via upload

---
+++ javaScript #1/script.js
@@ -0,0 +1,14 @@
+let Name = "Nika";
+let lastName = "Beglarashvili";
+let age = "26";
+const gender = "male";
+
+console.log(Name);
+console.log(lastName);
+console.log(age);
+console.log(gender);
+
+let rem = 101 % 2;
+
+
+console.log(rem);
